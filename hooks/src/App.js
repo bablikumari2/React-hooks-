05abce94ed5747a76053370bdefbe7d8b281e181
bablikumari2 <anandbabli96@gmail.com>
2022-08-01 23:23:45 +0530

    revision hook

---
+++ hooks/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ErrorExample from './Hooks/UseState/1-error-example';
+
+function App() {
+  return (
+    <div className="App">
+      <ErrorExample/>
+    </div>
+  );
+}
+
+export default App;
